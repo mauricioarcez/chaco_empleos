d967ffc1220ad03c1dae0bccdef91fb29307d697
mauricioarcez <mauricioarcez23@gmail.com>
2023-07-30 03:05:08 -0300

    funcional todas las medidas y vistas de comentario

--- apps/comentarios/forms.py
+++ apps/comentarios/forms.py
@@ -6,3 +6,11 @@ class ComentarioForm(forms.ModelForm):
     class Meta:
         model = Comentario
         fields=['texto']
+        widgets = {
+            'texto': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3,
+                'style': 'max-width: 100%',
+                'placeholder': 'Escribe tu comentario aquí...',
+            }),
+        }
