dbb744f20caf17c2a748a0f20418be49dfd25c45
Mauricio Arce <108899995+mauricioarcez@users.noreply.github.com>
2023-08-05 02:08:08 -0300

    docstrings en las vistas usuarios

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -8,6 +8,22 @@ from .forms import RegistrarUsuarioForm
 # Create your views here.
 
 class RegistrarUsuario(CreateView):
+    """
+    Vista basada en clase para registrar un nuevo usuario.
+
+    Esta vista permite a los usuarios registrarse en el sitio web. Utiliza la clase CreateView
+    para mostrar un formulario de registro personalizado definido en el formulario RegistrarUsuarioForm.
+    Una vez que el formulario es enviado y válido, el nuevo usuario será creado en la base de datos.
+    Si el registro es exitoso, el usuario será redirigido a la página de inicio de sesión.
+
+    Attributes:
+        model (Usuario): El modelo Usuario utilizado para crear un nuevo usuario.
+        template_name (str): El nombre del template utilizado para renderizar la página de registro.
+        form_class (RegistrarUsuarioForm): La clase del formulario utilizado para el registro del usuario.
+        success_url (str): La URL a la que se redirige al usuario después de un registro exitoso.
+
+    """
+    
     model = Usuario
     template_name = 'usuarios/registrar.html'
     form_class = RegistrarUsuarioForm
