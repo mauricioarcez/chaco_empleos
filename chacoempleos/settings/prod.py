26c6c6bcc58485da83b0b4e0c94f85428d392dc4
mauricioarcez <mauricioarcez23@gmail.com>
2023-09-08 01:37:33 -0300

    base, local, prod carpetas

--- chacoempleos/settings/prod.py
+++ chacoempleos/settings/prod.py
@@ -1,4 +1,12 @@
+from chacoempleos.settings.base import *
+from chacoempleos.logging import *
+from dotenv import load_dotenv
+import os
+
+load_dotenv(Path.joinpath(BASE_DIR, '.env'))
 DEBUG = False
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = ['mauricioarcez.pythonanywhere.com']
 
@@ -8,10 +16,13 @@ ALLOWED_HOSTS = ['mauricioarcez.pythonanywhere.com']
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": 'mauricioarcez$default',
-        "USER": 'mauricioarcez',
-        "PASSWORD": '152318mau',
-        "HOST": 'mauricioarcez.mysql.pythonanywhere-services.com',
-        "PORT": '3306',
+        "NAME": os.environ.get('DB_NAME'),
+        "USER": os.environ.get('DB_USER'),
+        "PASSWORD": os.environ.get('DB_PASSWORD'),
+        "HOST": os.environ.get('DB_HOST'),
+        "PORT": os.environ.get('DB_PORT'),
     }
 }
+
+STATIC_ROOT = Path.joinpath(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
